aac855aa9dc6ba9281ac9058f48875c36512ef8e
shifu013 <130575425+shifu013@users.noreply.github.com>
2023-12-18 08:19:12 +0800

    asd

--- app/views/Admin/room.php
+++ app/views/Admin/room.php
@@ -166,7 +166,7 @@
 							<a class="dropdown-item" href="faq.html"
 								><i class="dw dw-help"></i> Help</a
 							>
-							<a class="dropdown-item" href="login.html"
+							<a class="dropdown-item" href="logout"
 								><i class="dw dw-logout"></i> Log Out</a
 							>
 						</div>
